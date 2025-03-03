b070b824f2d9fec9b1ccc5d2192331afb7d7c5a3
Reza <rezahandzalah@gmail.com>
2025-03-03 12:10:45 +0700

    expect fail

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnet_actions;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Assert.Pass();
+        Assert.Fail();
+    }
+}
